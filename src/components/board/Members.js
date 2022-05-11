d8d0d9b2c8dd8d9d19a894ae929248fbabc78830
mirasrobert <mrmirasrobert@gmail.com>
2022-05-11 08:41:35 +0800

    Fix Add Member

--- src/components/board/Members.js
+++ src/components/board/Members.js
@@ -1,39 +1,50 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useSelector, useDispatch } from 'react-redux';
-import { addMember } from '../../actions/board';
-import getInitials from '../../utils/getInitials';
-import { TextField, Button } from '@material-ui/core';
-import Avatar from '@material-ui/core/Avatar';
-import Tooltip from '@material-ui/core/Tooltip';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import CloseIcon from '@material-ui/icons/Close';
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useSelector, useDispatch } from 'react-redux'
+import { addMember } from '../../actions/board'
+import getInitials from '../../utils/getInitials'
+import { TextField, Button } from '@material-ui/core'
+import Avatar from '@material-ui/core/Avatar'
+import Tooltip from '@material-ui/core/Tooltip'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import CloseIcon from '@material-ui/icons/Close'
 
 const Members = () => {
-  const [inviting, setInviting] = useState(false);
-  const [user, setUser] = useState(null);
-  const [inputValue, setInputValue] = useState('');
-  const [users, setUsers] = useState([]);
-  const boardMembers = useSelector((state) => state.board.board.members);
+  const [inviting, setInviting] = useState(false)
+  const [user, setUser] = useState(null)
+  const [inputValue, setInputValue] = useState('')
+  const [users, setUsers] = useState([])
+  const boardMembers = useSelector((state) => state.board.board.members)
   const searchOptions = users.filter((user) =>
-    boardMembers.find((boardMember) => boardMember.user === user._id) ? false : true
-  );
-  const dispatch = useDispatch();
+    boardMembers.find((boardMember) => boardMember.user === user._id)
+      ? false
+      : true
+  )
+  const dispatch = useDispatch()
 
   const handleInputValue = async (newInputValue) => {
-    setInputValue(newInputValue);
+    setInputValue(newInputValue)
     if (newInputValue && newInputValue !== '') {
-      const search = (await axios.get(`/api/users/${newInputValue}`)).data.slice(0, 5);
-      setUsers(search && search.length > 0 ? search : []);
+      const config = {
+        headers: {
+          'x-auth-token': localStorage.getItem('token'),
+        },
+      }
+
+      const { data } = await axios.get(`/api/users/${newInputValue}`, config)
+
+      const search = data?.slice(0, 4)
+
+      setUsers(search && search.length > 0 ? search : [])
     }
-  };
+  }
 
   const onSubmit = async () => {
-    dispatch(addMember(user._id));
-    setUser(null);
-    setInputValue('');
-    setInviting(false);
-  };
+    dispatch(addMember(user._id))
+    setUser(null)
+    setInputValue('')
+    setInviting(false)
+  }
 
   return (
     <div className='board-members-wrapper'>
@@ -43,11 +54,14 @@ const Members = () => {
             <Tooltip title={member.name} key={member.user}>
               <Avatar className='avatar'>{getInitials(member.name)}</Avatar>
             </Tooltip>
-          );
+          )
         })}
       </div>
       {!inviting ? (
-        <Button className='invite' variant='contained' onClick={() => setInviting(true)}>
+        <Button
+          className='invite'
+          variant='contained'
+          onClick={() => setInviting(true)}>
           Invite
         </Button>
       ) : (
@@ -56,12 +70,18 @@ const Members = () => {
             value={user}
             onChange={(e, newMember) => setUser(newMember)}
             inputValue={inputValue}
-            onInputChange={(e, newInputValue) => handleInputValue(newInputValue)}
+            onInputChange={(e, newInputValue) =>
+              handleInputValue(newInputValue)
+            }
             options={searchOptions}
             getOptionLabel={(member) => member.email}
             className='search-member'
             renderInput={(params) => (
-              <TextField {...params} helperText='Search for user by email' autoFocus />
+              <TextField
+                {...params}
+                helperText='Search for user by email'
+                autoFocus
+              />
             )}
           />
           <div className='add-member'>
@@ -69,8 +89,7 @@ const Members = () => {
               disabled={!user}
               variant='contained'
               color='primary'
-              onClick={onSubmit}
-            >
+              onClick={onSubmit}>
               Add Member
             </Button>
             <Button onClick={() => setInviting(false)}>
@@ -80,7 +99,7 @@ const Members = () => {
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Members;
+export default Members
